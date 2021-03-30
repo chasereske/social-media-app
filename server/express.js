30a317f6577d8860bd5740c5f0697e1f84cea332
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-29 20:38:43 -0400

    Remove bodyParser import from express.js due to update.

--- server/express.js
+++ server/express.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
@@ -7,8 +6,8 @@ import helmet from 'helmet';
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
