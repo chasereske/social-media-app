741da9aec169c3b8741ae5d7dcbe3d8426c7a295
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-29 20:43:10 -0400

    Refactor index.js to include express.js code.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ const connectDB = require("./starter/db");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import helmet from 'helmet';
 
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
 app.use(cors());
 
 // ASSIGN PORT
