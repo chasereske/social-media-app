785ee735bdccc12bfefc3ba026b2a54b630f337a
cdevjarrell <christian.j.jarrell@gmail.com>
2021-04-05 20:03:47 -0400

    Create signin component.

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import User from "./user/Users";
 import Signup from "./user/Signup";
+import Signin from "./auth/Signin";
 
 const MainRouter = () => {
   return (
@@ -11,6 +12,7 @@ const MainRouter = () => {
         <Route exact path="/" component={Home} />
         <Route path="/Users" component={User} />
         <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
       </Switch>
     </div>
   );
